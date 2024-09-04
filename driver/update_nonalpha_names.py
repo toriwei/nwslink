6cb4b20df88e73f36b6d76158cbc58c35b29f717
toriwei <toriw0208@gmail.com>
2024-09-04 16:06:02 -0700

    short fix for normalizing characters

---
+++ driver/update_nonalpha_names.py
@@ -0,0 +1,17 @@
+from unidecode import unidecode
+from driver import get_nonalpha_names, update_player_names
+
+
+try:
+  nonalpha_names = get_nonalpha_names()
+
+  updates = [{"player_id": player.get('player_id'), "current_name": player.get('name'), "updated_name": (unidecode(player.get('name'))) } for player in nonalpha_names]
+
+  for update in updates:
+    print(update)
+
+  result = update_player_names(updates)
+  print(result)
+
+except ValueError:
+  print(f'Sorry, something went wrong. Please ensure that the information is correct')
