2197971d81d535a88ee1e79749a6bc013d0f8d40
toriwei <toriw0208@gmail.com>
2024-11-20 12:04:53 -0800

    updating to new driver file name

--- driver/app.py
+++ driver/app.py
@@ -19,7 +19,7 @@ def ping():
 @app.route('/random_player', methods=['GET'])
 def get_random_player():
     app.logger.info('Random player endpoint accessed')
-    player = driver.get_random_player()
+    player = nwsl_driver.get_random_player()
     if player:
         return jsonify({"player": player["name"]})
     else:
@@ -33,7 +33,7 @@ def get_random_played_for():
     if not player:
         return jsonify({"error": "Player parameter is required"}), 400
     
-    random_team = driver.get_random_played_for(player, team=team)
+    random_team = nwsl_driver.get_random_played_for(player, team=team)
     
     if random_team:
         return jsonify({"team": random_team["t"]["team"], "seasons": random_team["f"]["seasons"]})
@@ -48,7 +48,7 @@ def get_random_teammate():
     if not team or not season:
         return jsonify({"error": "Team and season parameters are required"}), 400
     
-    teammate = driver.get_random_teammate(team, season)
+    teammate = nwsl_driver.get_random_teammate(team, season)
     if teammate:
         return jsonify({"teammate": teammate["name"]})
     else:
@@ -57,13 +57,13 @@ def get_random_teammate():
 @app.route('/is_valid_player', methods=['GET'])
 def is_valid_player():
     name = request.args.get('name')
-    is_valid_player = driver.is_valid_player(name)
+    is_valid_player = nwsl_driver.is_valid_player(name)
     return jsonify({"is_valid_player": bool(is_valid_player)})
 
 @app.route('/is_valid_team_name', methods=['GET'])
 def is_valid_team_name():
     team = request.args.get('team')
-    is_valid_team_name = driver.is_valid_team_name(team)
+    is_valid_team_name = nwsl_driver.is_valid_team_name(team)
     return jsonify({"is_valid_team_name": bool(is_valid_team_name)})
 
 if __name__ == "__main__":
